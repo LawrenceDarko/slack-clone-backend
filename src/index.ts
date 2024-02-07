5215332645b01603784bab6cccbaf83a2e8558ed
LawrenceDarko <darkolawrence@gmail.com>
2024-02-07 19:42:25 +0000

    cors

--- src/index.ts
+++ src/index.ts
@@ -19,14 +19,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:3000', 'https://slack-clone-frontend-bice.vercel.app/'],
+        origin: ['http://localhost:3000', 'https://slack-clone-frontend-bice.vercel.app'],
         // methods: ['GET', 'POST'],
         credentials: true,
     },
 });
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://slack-clone-frontend-bice.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://slack-clone-frontend-bice.vercel.app'],
     credentials: true,
 }));
 
